7dd1f6c0b2532834bb792be532873dd5bd15e6fa
ozzytao <taoyaguang@gmail.com>
2020-10-09 16:24:49 +1100

    compiled

---
+++ env.sh
@@ -0,0 +1,4 @@
+export PATH=$PATH:/home/yaguang/Projects/mln/ProbCog/apps
+export PYTHONPATH=$PYTHONPATH:/home/yaguang/Projects/mln/ProbCog/src/main/python
+export JYTHONPATH=$JYTHONPATH:/home/yaguang/Projects/mln/ProbCog/src/main/jython:/home/yaguang/Projects/mln/ProbCog/src/main/python
+export PROBCOG_HOME=/home/yaguang/Projects/mln/ProbCog
